fc569d4075e1fe33d12708b3a4eb46784201a147
ZeSantos0 <ze.santos0090@gmail.com>
2022-09-26 22:27:00 +0100

    PUTA QUE PARIU

--- DAD/BankServer/Program.cs
+++ DAD/BankServer/Program.cs
@@ -13,16 +13,55 @@ namespace BankPaxosClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static List<GrpcChannel> paxosChannels = new List<GrpcChannel>();
+        private readonly object balanceLock = new object();
+        double balance = 0;
+        static async Task Main(string[] args)
         {
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:1001");
             var client = new BankPaxosService.BankPaxosServiceClient(channel);
-            GreetRequest request = new GreetRequest{ Hi = true };
+            GrpcChannel channel2 = GrpcChannel.ForAddress("http://localhost:1002");
+            var client2 = new BankPaxosService.BankPaxosServiceClient(channel2);
+            GrpcChannel channel3 = GrpcChannel.ForAddress("http://localhost:1003");
+            var client3 = new BankPaxosService.BankPaxosServiceClient(channel3);
+
+            paxosChannels.Add(channel);
+            paxosChannels.Add(channel2);
+            paxosChannels.Add(channel3);
+
+            GreetRequest request = new GreetRequest { Hi = true };
             Console.WriteLine("Press any key to contact the server");
             Console.ReadKey();
-            client.Greeting(request);
+            var reply = await client.GreetingAsync(request);
+            Console.WriteLine($"{reply.Hi}");
             Console.WriteLine("The client will stop.");
+            Console.ReadKey();
+        }
+
+        private void deposit(double value)
+        {
+            lock (balanceLock)
+            {
+                balance += value;
+            }
+        }
+        private bool withdrawal(double value)
+        {
+            bool success = false;
+            lock (balanceLock)
+            {
+                if (balance < value)
+                {
+                    success = false;
+                }
+                else
+                {
+                    balance = balance - value;
+                    success = true;
+                }
+            }
+            return success;
         }
     }
 }

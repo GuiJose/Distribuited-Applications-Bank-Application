f8c68711210d82630aaf47e92f7c7552e529becb
Guilherme Santos <101069736+GuiJose@users.noreply.github.com>
2022-09-26 14:11:55 +0100

    Add files via upload

---
+++ DAD/BankServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankPaxosClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:1001");
+            var client = new BankPaxosService.BankPaxosServiceClient(channel);
+            GreetRequest request = new GreetRequest{ Hi = true };
+            Console.WriteLine("Press any key to contact the server");
+            Console.ReadKey();
+            client.Greeting(request);
+            Console.WriteLine("The client will stop.");
+        }
+    }
+}
---
+++ DAD/PaxosServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaxosServer
+{
+    public class ServerService : BankPaxosService.BankPaxosServiceBase
+    {
+        public ServerService()
+        {
+        }
+
+        public override Task<GreetReply> Greeting(
+            GreetRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(Reg(request));
+        }
+
+        public GreetReply Reg(GreetRequest request)
+        {
+            Console.WriteLine("Received an hi from bank server.");
+            return new GreetReply
+            {
+                Hi = true
+            };
+        }
+    }
+    class Program
+    {
+        const int Port = 1001;
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { BankPaxosService.BindService(new ServerService()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("PaxosServer server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}

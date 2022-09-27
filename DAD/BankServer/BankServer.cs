6a93dfcb6af585b5919715b8d9c0fa1395eb35f9
DiogoAPFernandes <72624875+DiogoAPFernandes@users.noreply.github.com>
2022-09-27 19:19:03 +0100

    perfect channels

---
+++ DAD/BankServer/BankServer.cs
@@ -0,0 +1,96 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DADPerfectChannel
+{
+    // ChatServerService is the namespace defined in the protobuf
+    // ChatServerServiceBase is the generated base implementation of the service
+    public class DADPerfectChannelService : PerfectChannelService.PerfectChannelServiceBase
+    {
+        private Dictionary<string, string> clientMap = new Dictionary<string, string>();
+
+        public DADPerfectChannelService()
+        {
+        }
+
+        public override Task<PerfectChannelReply> Test(
+            PerfectChannelRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(Reg(request));
+        }
+
+        public PerfectChannelReply Reg(PerfectChannelRequest request)
+        {
+
+            lock (this)
+            {
+                Console.WriteLine($"Received request with message: {request.Message}");
+            }
+            return new PerfectChannelReply
+            {
+                Status = true
+            };
+        }
+    }
+    class DADPerfectChannelServer
+    {
+        static void Main(string[] args)
+        {
+            const int ServerPort = 1001;
+            const string ServerHostname = "localhost";
+
+            Server server = new Server
+            {
+                Services = { PerfectChannelService.BindService(new DADPerfectChannelService()).Intercept(new ServerInterceptor()) },
+                Ports = { new ServerPort(ServerHostname, ServerPort, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("ChatServer server listening on port " + ServerPort);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+
+        }
+    }
+    public class ServerInterceptor : Interceptor
+    {
+
+        public override Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            string callId = context.RequestHeaders.GetValue("dad");
+            Console.WriteLine("DAD header: " + callId);
+            return base.UnaryServerHandler(request, context, continuation);
+        }
+
+    }
+
+    private void deposit(double value)
+    {
+        lock (balanceLock)
+        {
+            balance += value;
+        }
+    }
+    private bool withdrawal(double value)
+    {
+        bool success = false;
+        lock (balanceLock)
+        {
+            if (balance < value)
+            {
+                success = false;
+            }
+            else
+            {
+                balance = balance - value;
+                success = true;
+            }
+        }
+        return success;
+    }
+}
+
---
+++ DAD/Client/Client.cs
@@ -0,0 +1,63 @@
+﻿using DADPerfectChannelClient;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DADPerfectChannel
+{
+    static class DADPerfectChannelClient
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            const int ServerPort = 1001;
+            const string ServerHostname = "localhost";
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var clientInterceptor = new ClientInterceptor();
+            GrpcChannel channel = GrpcChannel.ForAddress("http://" + ServerHostname + ":" + ServerPort);
+            CallInvoker interceptingInvoker = channel.Intercept(clientInterceptor);
+            var client = new PerfectChannelService.PerfectChannelServiceClient(interceptingInvoker);
+            PerfectChannelRequest registerRequest = new PerfectChannelRequest { Message = "test" };
+            PerfectChannelReply reply = client.Test(registerRequest);
+            Console.WriteLine("reply: " + reply.Status);
+            Console.ReadKey();
+        }
+    }
+
+    public class ClientInterceptor : Interceptor
+    {
+        // private readonly ILogger logger;
+
+        //public GlobalServerLoggerInterceptor(ILogger logger) {
+        //    this.logger = logger;
+        //}
+
+        public override TResponse BlockingUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+
+            Metadata metadata = context.Options.Headers; // read original headers
+            if (metadata == null) { metadata = new Metadata(); }
+            metadata.Add("dad", "dad-value"); // add the additional metadata
+
+            // create new context because original context is readonly
+            ClientInterceptorContext<TRequest, TResponse> modifiedContext =
+                new ClientInterceptorContext<TRequest, TResponse>(context.Method, context.Host,
+                    new CallOptions(metadata, context.Options.Deadline,
+                        context.Options.CancellationToken, context.Options.WriteOptions,
+                        context.Options.PropagationToken, context.Options.Credentials));
+            Console.Write("calling server...");
+            TResponse response = base.BlockingUnaryCall(request, modifiedContext, continuation);
+            return response;
+        }
+    }
+}
